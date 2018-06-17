598babba4a6487a7c965f8607cc6f0f32426cdaf
geracimov <geracimov@gmail.com>
2018-06-17 23:17:50 +0500

    added class Socnet

--- src/main/java/Socnet.java
+++ src/main/java/Socnet.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Socnet {
     private Socnet socnet;
 
@@ -7,5 +10,10 @@ public class Socnet {
     public Socnet getInstance() {
         if (socnet == null) socnet = new Socnet();
         return socnet;
+    }
+
+
+    public List<User> findUsers(String searchString) {
+        return new ArrayList<User>();
     }
 }
